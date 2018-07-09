0fb989818f8cf7345f83e0be9a0277ad8e3dee0a
Rebecca Govert <becci.govert@gmail.com>
2018-07-09 21:07:46 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,5 +4,7 @@ def run_guessing_game
   while user_guess
     puts "Guess a number between 1 and 6."
     user_guess = gets.chomp
-    
+    computer_random = rand(1..6)
+    case user_guess
+      
 end
