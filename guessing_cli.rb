195b657a73206bc963602ae1c14c355643c37a4f
Rebecca Govert <becci.govert@gmail.com>
2018-07-09 21:11:56 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,7 +4,7 @@ def run_guessing_game
   while user_guess
     puts "Guess a number between 1 and 6."
     user_guess = gets.chomp
-    computer_random = rand(1..6)
+    computer_random = rand(1..6).to_s
     case user_guess.chomp
       when computer_random
         puts "You guessed the correct number!"
